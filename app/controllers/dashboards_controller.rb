9d6a328f9b705e9c7bf7a83e9abbc401fbd01913
Alejandra <marialesr.17@gmail.com>
2021-04-14 09:16:11 -0600

    new modification in controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,6 @@
 class DashboardsController < ApplicationController
   def index
     @coffee_in_month = Coffee.group_by_month(:created_at).count
+    @coffee_by_month_quantity = Coffee.group_by_month(:created_at).sum(:amount)
   end
 end
